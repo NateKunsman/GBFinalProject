61ddb72cb7af3eebdf90c1bad8a3147d0197dc4a
NateKunsman <nkkuns@protonmail.com>
2021-10-04 15:19:24 -0400

    added more to program UI and made changes to POCO

--- Cafe/02_Program UI.cs
+++ Cafe/02_Program UI.cs
@@ -9,6 +9,7 @@ namespace Cafe
     class Program_UI
     {
         private MenuRepository _repo = new MenuRepository();
+        // public void Run() is a method that runs/starts the application
         public void Run()
         {
             RunMenu();
@@ -20,14 +21,16 @@ namespace Cafe
             while (isRunning)
             {
                 Console.Clear();
+                //Display options to the user
                 Console.WriteLine(
                     "Enter the number of your selection:\n" +
                     "1. Create new menu item\n" +
                     "2. Show all menu items\n" +
                     "3. Delete Menu Item\n" +
                     "4. Exit");
+                //Get the users input
                 string userInput = Console.ReadLine();
-
+                //Evaluate the user's input and act accordingly
                 switch (userInput)
                 {
                     case "1":
@@ -44,9 +47,16 @@ namespace Cafe
                         break;
                     case "4":
                         //Exit
+
                         isRunning = false;
                         break;
+                    default:
+                        Console.WriteLine("Please enter a valid number");
+                        break;
                 }
+                Console.WriteLine("Please press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
         //Create Item
@@ -55,12 +65,57 @@ namespace Cafe
             Console.Clear();
             Menu menu = new Menu();
             //MealNumber
-            Console.WriteLine("Please enter the number this menu item will be refered to by");
-            menu.MealNumber = Console.ReadLine();
+            Console.WriteLine("Enter the number this menu item will be refered to by");
+            menu.MealNumber = int.Parse(Console.ReadLine());
             //MealName
+            Console.WriteLine("Endter the name of this meal");
+            menu.MealName = Console.ReadLine();
             //MealDesciption
+            Console.WriteLine("Enter a description for this meal");
+            menu.MealDescription = Console.ReadLine();
             //MealIngredients
+            Console.WriteLine("Enter ingredients for this meal\n" +
+                "Please type each ingredient followed by a comma.\n" +
+                "Example: cheese, lettuce, tomato");
+            string mealIngredients = Console.ReadLine();
+            menu.MealIngredients = mealIngredients.Split(',').ToList);
             //MealPrice
+            Console.WriteLine("Enter price for this meal");
+           menu.MealPrice = double.Parse(Console.ReadLine());
+
+            _repo.CreatNewMenuItem(menu);
+            
+
+        }
+        //Show all Menu Items
+        private void ShowAllMenuItems()
+        {
+            List<Menu> listOfMenuItems = _repo.ShowMenuItems();
+            foreach(Menu menu in listOfMenuItems)
+            {
+                Console.WriteLine($"Meal Number: {menu.MealNumber}\n," +
+                    $" Meal Name: {menu.MealName}\n," +
+                    $" Description: {menu.MealDescription}\n," +
+                    $" Ingredients: {menu.MealIngredients}," +
+                    $" Price: {menu.MealPrice}");
+            }
+        }
+        //Delete Menu Items
+        private void DeleteMenuItem()
+        {
+
+        }
+
+
+
+        //Helper Methods
+        private void SeedData()
+        {
+            //*********NEEDS FINISHED**********
+        }
+        private void DisplayMenu()
+        {
+
         }
     }
 }
--- Cafe/03_MenuPOCO.cs
+++ Cafe/03_MenuPOCO.cs
@@ -11,7 +11,17 @@ namespace Cafe
         public int MealNumber { get; set; }
         public string MealName { get; set; }
         public string MealDescription { get; set; }
-        public string[] MealIngredients { get; set; }
+        public List<String> MealIngredients { get; set; }
         public double MealPrice { get; set; }
+        public Menu() { }
+
+        public Menu(int mealNumber, string mealName, string mealDescription, List<String> mealIngredients, double mealPrice)
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            MealDescription = mealName;
+            MealIngredients = mealIngredients;
+            MealPrice = mealPrice;
+        }
     }
 }
